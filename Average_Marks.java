68a90eea288c7bef9291a4564aea950de8de9126
Saksham Saxena <38011670+sakshamsaxena22@users.noreply.github.com>
2025-06-16 09:22:26 +0530

    Added Day_1 code

---
+++ Average_Marks.java
@@ -0,0 +1,13 @@
+package Day_1;
+
+public class Average_Marks {
+	public static void main(String[]args) {
+//	Sam’s mark in Maths is 94, Physics is 95 and Chemistry is 96 out of 100. Find the average percent mark in PCM
+//	I/P => NONE
+//	O/P => Sam’s average mark in PCM is ___ 
+ double maths=94, Physics=95,Chemistry=96;
+ double Avg= (maths+Physics+Chemistry) /3;
+ System.out.println("Sam’s average mark in PCM is" +Avg); 
+	}
+
+}
